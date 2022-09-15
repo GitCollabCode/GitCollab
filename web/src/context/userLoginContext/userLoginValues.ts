67729d9fe0cd1cd2585bbd738022abc038647620
KevinTheFox <kevin.fox@applydigital.co>
2022-09-15 17:10:44 -0400

    Part 1 github login

---
+++ web/src/context/userLoginContext/userLoginValues.ts
@@ -0,0 +1,51 @@
+import { userValue } from '../../constants/common'
+
+const isLoggedLS = localStorage.getItem('isLoggedIn')
+const user = localStorage.getItem('user')
+
+export const initialState = {
+  isLoggedIn: isLoggedLS ? JSON.parse(isLoggedLS) : false,
+  user: user ? JSON.parse(user) : null,
+  client_id: process.env.REACT_APP_CLIENT_ID,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  client_secret: process.env.REACT_APP_CLIENT_SECRET,
+  proxy_url: process.env.REACT_APP_PROXY_URL,
+}
+
+type LogIn = {
+  type: 'LOGIN'
+  payload: any
+}
+
+type LogOut = {
+  type: 'LOGOUT'
+}
+
+export type ValidAction = LogIn | LogOut
+
+export const reducer = (state: userValue, action: ValidAction): userValue => {
+  switch (action.type) {
+    case 'LOGIN': {
+      localStorage.setItem(
+        'isLoggedIn',
+        JSON.stringify(action.payload.isLoggedIn)
+      )
+      localStorage.setItem('user', JSON.stringify(action.payload.user))
+      return {
+        ...state,
+        isLoggedIn: action.payload.isLoggedIn,
+        user: action.payload.user,
+      }
+    }
+    case 'LOGOUT': {
+      localStorage.clear()
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+      }
+    }
+    default:
+      return state
+  }
+}
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { UserLoginProvider } from './Context/userLoginContext/userLoginContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <UserLoginProvider>
+    <App />
+  </UserLoginProvider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
