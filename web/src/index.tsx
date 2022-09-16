3b8bfc64ec40ea16a9a1ef7724a2bd0ab7872d13
robotevan <robotevan24@gmail.com>
2022-09-15 21:25:19 -0400

    fixing react include path, adding volume mount for frontend dir

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { UserLoginProvider } from './Context/userLoginContext/userLoginContext'
+import { UserLoginProvider } from './context/userLoginContext/userLoginContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
