f827893b029fa76799aa176414d74e3de903ad64
robotevan <robotevan24@gmail.com>
2022-09-21 15:23:07 -0400

    removing logger from db

--- internal/db/db.go
+++ internal/db/db.go
@@ -6,15 +6,13 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/labstack/echo/v4"
 )
 
 type PostgresDriver struct {
-	Log        *echo.Logger
 	Connection pgx.Conn
 }
 
-func ConnectPostgres(Log echo.Logger) (*PostgresDriver, error) {
+func ConnectPostgres() (*PostgresDriver, error) {
 	postgresUrl := os.Getenv("POSTGRES_URL")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
@@ -24,6 +22,5 @@ func ConnectPostgres(Log echo.Logger) (*PostgresDriver, error) {
 		return nil, err
 	}
 
-	Log.Info("Connected to postgres db")
-	return &PostgresDriver{Connection: *conn, Log: &Log}, err
+	return &PostgresDriver{Connection: *conn}, err
 }
