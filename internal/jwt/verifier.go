96eb046ae924fea4cf26fe8b6645e7e66af38834
robotevan <robotevan24@gmail.com>
2022-09-28 15:46:39 -0400

    removing some cringe

--- internal/jwt/verifier.go
+++ internal/jwt/verifier.go
@@ -10,27 +10,24 @@ import (
 func VerifyJWT(logger *logrus.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			token := r.Header.Get("Token")
-			if token == "" { // empty token
-				w.WriteHeader(http.StatusUnauthorized)
-				return
-			}
-			if r.Header["Token"] != nil {
+			if r.Header["Token"] != nil { // get token from header
 				token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
-					if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+					if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok { // if no
 						w.WriteHeader(http.StatusUnauthorized)
 						_, err := w.Write([]byte("User unauthorized!"))
 						return nil, err // set error?
 					}
 					return "", nil
 				})
+
 				if err != nil {
 					w.WriteHeader(http.StatusUnauthorized)
-
 				}
+
 				if token.Valid { // we gucci, token is valid frfr
 					next.ServeHTTP(w, r)
 				}
+
 			} else { // no token in header
 				w.WriteHeader(http.StatusUnauthorized)
 				_, err := w.Write([]byte("You're Unauthorized due to No token in the header"))
@@ -38,7 +35,6 @@ func VerifyJWT(logger *logrus.Logger) func(http.Handler) http.Handler {
 					return
 				}
 			}
-
 		}
 		return http.HandlerFunc(fn)
 	}
