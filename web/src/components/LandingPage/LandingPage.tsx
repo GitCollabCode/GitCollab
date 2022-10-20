2309a79d18a9eec579090cff8ea328b82e4a3806
KevinTheFox <kevin.fox@applydigital.co>
2022-10-20 16:34:14 -0400

    Landing page edits

--- web/src/components/LandingPage/LandingPage.tsx
+++ web/src/components/LandingPage/LandingPage.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import styles from './LandingPage.module.css'
+import style from './LandingPage.module.css'
 
 const LandingPage = () => {
-  return <div className={styles.container}></div>
+  return (
+    <>
+      <div className={style.container}>
+        <div className={style.textContainer}>
+          <div className={style.topText}>Coding is</div>
+          <div className={style.animatedText}>
+            <div className={style.line}>wagwan</div>
+            <div className={style.line}>cringe</div>
+            <div className={style.line}>collaborative</div>
+            <div className={style.line}>in your walls</div>
+            <div className={style.line}>maged sus</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default LandingPage
