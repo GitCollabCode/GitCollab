c88f6193f2d496d4ecd12f75df304a838ecf1c5f
robotevan <robotevan24@gmail.com>
2022-11-27 22:30:40 -0500

    adding projects db, fixing routes for projects, setting profile id to unique

--- microservices/projects/data/projects_init.sql
+++ microservices/projects/data/projects_init.sql
@@ -1,73 +1,26 @@
+Create TYPE skill AS (
+    skill_id            INTEGER,
+    skill_description   VARCHAR
+);
+
 Create TABLE IF NOT EXISTS projects (
-    project_id            INTEGER         NOT NULL AUTO,
-    github_owner_id       INTEGER         NOT NULL,
+    project_id            SERIAL          NOT NULL,
+    project_owner         INTEGER         REFERENCES profiles,
     project_name          VARCHAR         NOT NULL,
     project_description   VARCHAR         NOT NULL,
+    project_skills        skill           [], 
     date_created          TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
     PRIMARY Key (project_id)
-
-    CONSTRAINT fk_projectOwner
-    FOREIGN KEY(github_owner_id) 
-    REFERENCES profiles(github_user_id)
 );
 
-Create TABLE IF NOT EXISTS skills (
-    skill_id            INTEGER         NOT NULL  AUTO,
-    skill_name          VARCHAR         NOT NULL, 
-    PRIMARY Key (skill_id)   
-);
-
-/*
- * Create skills for projects
- */
-INSERT INTO skills(skill_id, skill_name)
-values (default, "Frontend"),
-(default, "Backend"),
-(default, "Devops"),
-(default, "Testing"),
-(default, "Documentation");     
-
-/*
- * Associative table for projects->users
- */
-Create TABLE IF NOT EXISTS project_members(
-    id           INTEGER         NOT NULL AUTO,
-    project_id   INTEGER         NOT NULL,
-    user_id      INTEGER         NOT NULL,
-    user_role    INTEGER         NOT NULL,
-    PRIMARY KEY (id)
-
-    /* Foreign key for project that members are a part of */
-    CONSTRAINT fk_projectId
-    FOREIGN KEY(project_id)
-    REFERENCES projects(project_id)
-
-    /* Foreign key for profile that belongs to */
-    CONSTRAINT fk_userId
-    FOREIGN KEY(user_id)
-    REFERENCES profiles(user_id)
-)
-
-
-/* Create TABLE IF NOT EXISTS tasks (
-    task_id             VARCHAR       NOT NULL,
-    project_id          VARCHAR       NOT NULL,
-    task_creator_id     INTEGER       NOT NULL,
-    assignee_id         INTEGER       NOT NULL,            
+create TABLE IF NOT EXISTS tasks (
+    task_id             SERIAL        NOT NULL,
+    project_id          INTEGER       REFERENCES projects,
+    task_creator_id     INTEGER       REFERENCES profiles,
+    assignee_id         INTEGER       REFERENCES profiles,            
     date_created_date   DATE          NOT NULL,
     task_end_date       DATE          NOT NULL,
-    skills              VARCHAR[1][3] NOT NULL,
+    skills              skill         [],
     description         VARCHAR       NOT NULL,
-    PRIMARY Key (task_id)
-    CONSTRAINT fk_projects
-        FOREIGN KEY(project_id) 
-	        REFERENCES projects(project_id)
-
-    CONSTRAINT fk_taskCreator
-        FOREIGN KEY(task_creator_id)
-            REFERENCES profiles(github_user_id)
-
-    CONSTRAINT fk_taskAssignee
-        FOREIGN KEY(assignee_id)
-            REFERENCES profiles(github_user_id)  
-); */
+    PRIMARY KEY (task_id)
+);
