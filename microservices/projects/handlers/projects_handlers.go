bea3b7a9a20c88d0cd6718f7431a15ffe27a9d5c
Rodrigo <rodrigo_3500@live.com>
2023-01-12 23:09:45 -0500

    User Repos handler

--- microservices/projects/handlers/projects_handlers.go
+++ microservices/projects/handlers/projects_handlers.go
@@ -3,6 +3,11 @@ package handlers
 import (
 	"net/http"
 
+	githubAPI "github.com/GitCollabCode/GitCollab/internal/github"
+	jsonio "github.com/GitCollabCode/GitCollab/internal/jsonhttp"
+	"github.com/GitCollabCode/GitCollab/internal/models"
+	projectsModels "github.com/GitCollabCode/GitCollab/microservices/projects/models"
+
 	"github.com/GitCollabCode/GitCollab/internal/db"
 	"github.com/sirupsen/logrus"
 )
@@ -21,7 +26,31 @@ func NewProjects(pg *db.PostgresDriver, logger *logrus.Logger) *Projects {
 // retrieve list of github repos associated to a given user
 // Request all repos that a user owns on github. Will require valid access token
 func (p *Projects) GetUserRepos(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Not Implemented", http.StatusNotImplemented)
+	client := githubAPI.GetGitClientFromContext(r)
+
+	repos, err := githubAPI.GetUserOwnedRepos(client)
+	if err != nil {
+		p.Log.Errorf("GetUserOwnedRepos API call failed: %s", err.Error())
+		// NOTE: Repetative code, clean this up
+		w.WriteHeader(http.StatusInternalServerError)
+		err = jsonio.ToJSON(&models.ErrorMessage{Message: "internal server error"}, w)
+		if err != nil {
+			p.Log.Fatalf("GetProfile failed to send error response: %s", err)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	res := projectsModels.GetReposResp{
+		Repos: repos,
+	}
+
+	err = jsonio.ToJSON(res, w)
+	if err != nil {
+		p.Log.Fatalf("GetProfile failed to send response: %s", err)
+	}
 }
 
 // retrieve information about a given repo
--- microservices/projects/models/projects_models.go
+++ microservices/projects/models/projects_models.go
@@ -1,1 +1,7 @@
 package projectsModels
+
+import "github.com/google/go-github/github"
+
+type GetReposResp struct {
+	Repos []*github.Repository `json:"repos"`
+}
