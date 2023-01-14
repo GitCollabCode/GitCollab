d0f1ecf7637b323f3b2a87e8638e293661c6e8d0
Kevin <kevindafox@gmail.com>
2023-01-13 21:14:59 -0500

    Stuff

--- web/src/components/Profile/Profile.tsx
+++ web/src/components/Profile/Profile.tsx
@@ -3,6 +3,7 @@ import Media from 'react-media'
 import styles from './Profile.module.css'
 import { GET_PROFILE } from '../../constants/endpoints'
 import { profileResponse } from '../../constants/common'
+import Table, { rowType } from '../Table/Tables'
 
 const Profile = () => {
   let initialProfile: profileResponse = {
@@ -31,6 +32,14 @@ const Profile = () => {
         console.log(data)
       })
   }, [])
+
+  const intitalDataRows: rowType[] = [
+    {id : "1", date : "2014-04-18", total : 121.0, status : "Shipped", name : "A", points: 5, percent : 50},
+    {id : "2", date : "2014-04-21", total : 121.0, status : "Not Shipped", name : "B", points: 10, percent: 60},
+    {id : "3", date : "2014-08-09", total : 121.0, status : "Not Shipped", name : "C", points: 15, percent: 70},
+    {id : "4", date : "2014-04-24", total : 121.0, status : "Shipped", name : "D", points: 20, percent : 80},
+    {id : "5", date : "2014-04-26", total : 121.0, status : "Shipped", name : "E", points: 25, percent : 90},
+]
 
   return (
     <div className={styles.container}>
@@ -63,10 +72,17 @@ const Profile = () => {
             <div className={styles.card}>
               <div>Card 2</div>
               <div className={styles.line}></div>
-              <div>Card 2 Body</div>
+              <div>Card 2 Body
+              <Table rows={intitalDataRows} isExpandable={false} />
+              </div>
+              
             </div>
           </div>
+          
         </div>
+      
+          
+        
       </Media>
       <Media query={{ maxWidth: 1023 }}>
         <div className={styles.tables}>
